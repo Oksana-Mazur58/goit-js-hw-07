6d41bf9396cb463018184c12a4b465a508734734
Oksana-Mazur58 <72046543+Oksana-Mazur58@users.noreply.github.com>
2021-02-26 18:19:58 +0200

    Завдання 4

--- js/task_3.js
+++ js/task_3.js
@@ -8,43 +8,42 @@
 
 // Все элементы галереи должны добавляться в DOM за одну операцию вставки.
 // Добавь минимальное оформление галереи флексбоксами или гридами через css-классы.
-// const images = [
-//   {
-//     url:
-//             'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress & cs=tinysrgb& dpr=2 & h=750 & w=1260',
-//     alt: 'White and Black Long Fur Cat',
-//   },
-//   {
-//     url:
-//       'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-//     alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
-//   },
-//   {
-//     url:
-//       'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-//     alt: 'Group of Horses Running',
-//   },
-// ];
-
-
-// .insertAdjacentHTML(‘beforeend’,
-//   `<li class=“gallery__item” ><img class=“gallery__img” src=“${i.url}” alt=“${i.alt}“/></li>`);
+const images = [
+  {
+    url:
+            'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress & cs=tinysrgb& dpr=2 & h=750 & w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
 
 
 
-const createGallery = images.forEach(image=> {
+const imageGalleryRef = document.querySelector('#gallery')
+console.log(imageGalleryRef);
 
-    return
-    ` <li>
-        <img url = ${image.url} alt = ${image.alt}/>
-    </li>
-        
-    `;
- 
-  
-})
+const createItemOfGallery = value => {
+  const { url, alt } = value
+  return `<li class = ><img src='${url}' alt='${alt}' width = 600 height = 600/></li>`;
+}
+const сreateGallery = images.map(createItemOfGallery).join('');
+imageGalleryRef.insertAdjacentHTML('beforeend', сreateGallery)
 
-const navEl = document.querySelector('#gallery');
+console.log(createItemOfGallery)
 
-navEl.insertAdjacentHTML('beforeend', elementsOfGallery);
-console.log(navEl);
+
+// images.forEach(element => imageGalleryRef.insertAdjacentHTML('beforeend',
+//               `<li><img src=${element.url} alt=${element.alt} `)
+// );
+// imageGalleryRef.style.display = 'flex';
+// imageGalleryRef.style.justifyContent = 'space-between';
+// imageGalleryRef.style.listStyle = 'none';
